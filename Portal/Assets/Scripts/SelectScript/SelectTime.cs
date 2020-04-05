b491ebd52c55e795d9b1d081dcfa2db212d710cd
sashay <sashay@sfu.ca>
2020-04-05 13:20:54 -0700

    Added select adventure pagr

---
+++ Portal/Assets/Scripts/SelectScript/SelectTime.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectTime : MonoBehaviour
+{
+
+    public GameObject A1;
+    public GameObject A2;
+    public GameObject A3;
+    public GameObject A4;
+    public GameObject A5;
+
+    public GameObject slider;
+    // Start is called before the first frame update
+    public void ShowTimer()
+    {
+        A1.SetActive(false);
+        A2.SetActive(false);
+        A3.SetActive(false);
+        A4.SetActive(false);
+        A5.SetActive(false);
+
+
+        slider.SetActive(true);
+
+    }
+
+
+}
---
+++ Portal/Assets/Scripts/SelectScript/SliderText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderText : MonoBehaviour
+{
+    public GameObject timeSlider;
+    public GameObject text;
+    public float sliderValue = 0;
+    // Update is called once per frame
+    public void ChangeTextValue()
+    {
+        sliderValue = timeSlider.GetComponent<Slider>().value;
+        text.GetComponent<Text>().text = "Minutes: " + sliderValue;
+
+
+    }
+}
